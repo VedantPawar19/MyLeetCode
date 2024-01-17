9a6e7819fdfe821452272555a5147fc7ccc81067
VedantPawar19 <118812579+VedantPawar19@users.noreply.github.com>
2024-01-17 23:15:29 +0530

    Time: 19 ms (5.47%), Space: 42.3 MB (5.38%) - LeetHub

---
+++ 1252-cells-with-odd-values-in-a-matrix/1252-cells-with-odd-values-in-a-matrix.java
@@ -0,0 +1,49 @@
+class Solution {
+    public int oddCells(int m, int n, int[][] indices) {
+        int[][] matrix = new int[m][n];
+
+        // zero matrix
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = 0;
+            }
+        }
+        System.out.println(Arrays.deepToString(matrix));
+
+        // final matrix after increments
+        increment(m, n, indices, matrix);
+        
+        // finding odd elements count
+        int count = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if(matrix[i][j] % 2 != 0){
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    void increment(int m, int n, int[][] indices, int[][] matrix) {
+        int i = 0;
+        while(i<indices.length){
+            int j = 0; //col of indices
+            
+            for(int r = 0; r < matrix.length; r++){
+                for (int c = 0; c < matrix[r].length; c++) {
+                    if(r==indices[i][j]){
+                        matrix[r][c]++;
+                    }   
+                    if(c==indices[i][j+1]){
+                        matrix[r][c]++;
+                    }
+
+                }
+            }
+
+            i++;
+        }
+    }
+}
